c31c7ed8553841b26a82c93954deb46b6f2e44b9
merukimon <mneiter@gmail.com>
2025-06-08 11:57:00 +0300

    Removes unused imports and commented-out code
    Cleans up the code by removing commented-out imports and console logs.
    
    This improves code readability and maintainability by eliminating clutter.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 // import { isPalindrome, isPalindrome2 } from './palindrome';
 // import { twoSum } from './twoSum';
-
-import { romanToInt } from './romanToInt';
+// import { romanToInt } from './romanToInt';
 
 // console.log(romanToInt('III')); // 3
 // console.log(romanToInt('LVIII')); // 58
 // console.log(romanToInt('MCMXCIV')); // 1994
-console.log(romanToInt('DCXXI')); // 1994
+// console.log(romanToInt('DCXXI')); // 1994
 
 // console.log(twoSum([2, 7, 11, 15], 9)); // [0, 1]
 // console.log(twoSum([3, 2, 4], 6)); // [1, 2]
@@ -15,7 +14,3 @@ console.log(romanToInt('DCXXI')); // 1994
 // console.log(isPalindrome(100)); // true
 // console.log(isPalindrome(100)); // true
 // console.log(isPalindrome(-10)); // false
-
-// console.log(isPalindrome2(505)); // true
-// console.log(isPalindrome2(100)); // false
-// console.log(isPalindrome2(-10)); // false
